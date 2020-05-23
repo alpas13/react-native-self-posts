9fdaffbfa234fd0ddca7dda02fb5fb8ee0fca668
Alexey Pasmarnov <a.pasmarnov@gmail.com>
2020-05-23 12:30:18 +0200

    Implemented PostScreen

--- src/screens/MainScreen/MainScreen.js
+++ src/screens/MainScreen/MainScreen.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
+import {StyleSheet, View, Text, Button, FlatList} from 'react-native';
+import {DATA} from '../../mock/mock';
+import {Post} from "../../components/Post";
 
 export const MainScreen = ({navigation}) => {
-  const onPostOpen = () => {
-    navigation.navigate('Post');
+  const openPostHandler = (post) => {
+    navigation.navigate('Post', {postId: post.id});
   }
   return (
-      <View style={styles.center}>
-        <Text>
-          MainScreen
-        </Text>
-        <Button onPress={onPostOpen} title='Go to first post'/>
+      <View style={styles.wrapper}>
+        <FlatList data={DATA} renderItem={({item, }) =>
+            <Post post={item} onOpen={openPostHandler}/>} keyExtractor={(post) => post.id.toString()}
+        />
       </View>
   );
 };
@@ -20,9 +21,7 @@ MainScreen.navigationOptions = {
 };
 
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  wrapper: {
+    padding: 10,
   }
 });
--- src/screens/PostScreen/PostScreen.js
+++ src/screens/PostScreen/PostScreen.js
@@ -1,24 +1,67 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  Image,
+  Button,
+  Alert
+} from 'react-native';
+import {DATA} from "../../mock/mock";
 
-export const PostScreen = (props) => {
+export const PostScreen = ({navigation}) => {
+  const postId = navigation.getParam('postId');
+
+  const post = DATA.find((item) => item.id === postId);
+
+  const removePostHandler = () => {
+    Alert.alert(
+        "Remove post",
+        "Are you sure, that you want this post remove?",
+        [
+          {
+            text: "Cancel",
+            style: "cancel"
+          },
+          {
+            text: "Remove", onPress: () => {
+            }, style: 'destructive'
+          }
+        ],
+        {cancelable: false}
+    );
+  };
+
   return (
-      <View style={styles.center}>
-        <Text>
-          PostScreen
-        </Text>
-      </View>
+      <ScrollView>
+        <View>
+          <Image style={styles.image} source={{uri: post.img}}/>
+        </View>
+        <View style={styles.textWrap}>
+          <Text style={styles.text}>
+            {post.text}
+          </Text>
+        </View>
+        <Button onPress={removePostHandler} title={'Remove'} color={'red'}/>
+      </ScrollView>
   );
 };
 
-PostScreen.navigationOptions = {
-  headerTitle: 'First post',
-}
+PostScreen.navigationOptions = ({navigation}) => {
+  const postId = navigation.getParam('postId');
+  return {headerTitle: `Post ${postId}`}
+};
 
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+  image: {
+    width: '100%',
+    height: 200,
+  },
+  textWrap: {
+    padding: 10,
+  },
+  text: {
+    fontFamily: 'openSans-regular',
   }
 });
